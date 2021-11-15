2384140935a02377c5f162cb87e6f5075fc9e6bc
onAuspicious <wotj7687@icloud.com>
2021-11-15 18:06:36 +0900

    ThreadLocal 적용, 테스트

---
+++ src/test/java/hello/advanced/app/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,42 @@
+package hello.advanced.app.trace.threadlocal;
+
+import hello.advanced.app.trace.threadlocal.code.FieldService;
+import hello.advanced.app.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+        Runnable userB = () -> {
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-B");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-A");
+
+        threadA.start();
+        sleep(100);
+        threadB.start();
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
