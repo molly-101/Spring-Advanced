eab4cd1470e80c2fe14b9217e438786fb1cc8f7b
onAuspicious <wotj7687@icloud.com>
2021-11-16 22:04:03 +0900

    전략 패턴 테스트 코드 추가

---
+++ src/test/java/hello/advanced/app/trace/strategy/ContextV1Test.java
@@ -0,0 +1,64 @@
+package hello.advanced.app.trace.strategy;
+
+import hello.advanced.app.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.app.trace.strategy.code.strategy.Strategy;
+import hello.advanced.app.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.app.trace.strategy.code.strategy.StrategyLogic2;
+import hello.advanced.app.trace.template.code.AbstractTemplate;
+import hello.advanced.app.trace.template.code.SubClassLogic1;
+import hello.advanced.app.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void strategy() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        // business logic execute
+        log.info("비즈니스 로직 1 실행");
+        // business logic close
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        // business logic execute
+        log.info("비즈니스 로직 2 실행");
+        // business logic close
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비즈니스 로직 2 실행"));
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV2() {
+        ContextV1 contextV1 = new ContextV1(() -> log.info("비즈니스 로직 1 실행"));
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비즈니스 로직 2 실행"));
+        contextV2.execute();
+    }
+}
---
+++ src/test/java/hello/advanced/app/trace/strategy/code/strategy/ContextV2.java
@@ -0,0 +1,20 @@
+package hello.advanced.app.trace.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 전략을 파라미터로 전달 받는 방식
+ */
+@Slf4j
+public class ContextV2 {
+
+    public void execute(Strategy strategy) {
+        long startTime = System.currentTimeMillis();
+        // business logic execute
+        strategy.call(); // 위임
+        // business logic close
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+}
